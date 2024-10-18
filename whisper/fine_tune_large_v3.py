e8b826aea3357ad60465a23565db18fe16927391
Liting <litingzhou1119@outlook.com>
2024-10-18 12:57:01 -0400

    fine-tine: prepare data

--- whisper/fine_tune_large_v3.py
+++ whisper/fine_tune_large_v3.py
@@ -12,15 +12,30 @@ import os
 csv_file_path = '../../data_processed/dataset_splitted.csv'
 df = pd.read_csv(csv_file_path)
 
+# directory to save the processed dataset
+processed_data_path = '../../data_processed/processed_audio_dataset'
+final_processed_data_path = '../../data_processed/final_processed_dataset'
+
 def load_audio(batch):
     audio_file_path = os.path.join("../../data_processed/audios", batch["folder_name"], batch["file_cut"])
     audio, _ = sf.read(audio_file_path)
     batch["audio"] = {"array": audio}
     return batch
 
-dataset = Dataset.from_pandas(df)
-# map the function to load audio files
-dataset = dataset.map(load_audio)
+# check if the processed dataset already exists
+if os.path.exists(processed_data_path):
+    # load preprocessed dataset
+    dataset = Dataset.load_from_disk(processed_data_path)
+    print("Loaded dataset from disk!")
+else:
+    # create dataset from the CSV and map the function to load audio files
+    dataset = Dataset.from_pandas(df)
+    dataset = dataset.map(load_audio)
+    
+    # save the processed dataset for future use
+    dataset.save_to_disk(processed_data_path)
+    print("Processed dataset saved to disk!")
+
 print(dataset[0])
 
 feature_extractor = WhisperFeatureExtractor.from_pretrained("openai/whisper-small")
@@ -36,5 +51,16 @@ def prepare_dataset(batch):
     batch["labels"] = tokenizer(batch["transcriptions"]).input_ids
     return batch
 
-dataset = dataset.map(prepare_dataset, remove_columns=dataset.column_names["train"], num_proc=4)
+if os.path.exists(final_processed_data_path):
+    # load fully preprocessed dataset
+    dataset = Dataset.load_from_disk(final_processed_data_path)
+    print("Loaded final processed dataset from disk!")
+else:
+    # prepare the dataset
+    dataset = dataset.map(prepare_dataset, remove_columns=dataset.column_names, num_proc=8)
+    
+    # save the fully processed dataset for future use
+    dataset.save_to_disk(final_processed_data_path)
+    print("Final processed dataset saved to disk!")
 
+print("after prepare_data", dataset[0])
