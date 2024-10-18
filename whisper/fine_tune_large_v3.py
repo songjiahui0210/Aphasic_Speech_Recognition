ff26e83e86ba3f245797838b4c98b18c41369b75
Liting <litingzhou1119@outlook.com>
2024-10-18 14:24:43 -0400

    complete data preparation

--- whisper/fine_tune_large_v3.py
+++ whisper/fine_tune_large_v3.py
@@ -16,10 +16,24 @@ df = pd.read_csv(csv_file_path)
 processed_data_path = '../../data_processed/processed_audio_dataset'
 final_processed_data_path = '../../data_processed/final_processed_dataset'
 
+# list to keep track of missing audio files
+missing_files = []
+
 def load_audio(batch):
     audio_file_path = os.path.join("../../data_processed/audios", batch["folder_name"], batch["file_cut"])
-    audio, _ = sf.read(audio_file_path)
-    batch["audio"] = {"array": audio}
+    # check if the file exists
+    if os.path.exists(audio_file_path):
+        try:
+            audio, sample_rate = sf.read(audio_file_path)
+            batch["audio"] = {"array": audio, "sampling_rate": sample_rate}
+        except Exception as e:
+            print(f"Error loading {audio_file_path}: {e}")
+            batch["audio"] = None
+    else:
+        print(f"File not found: {audio_file_path}")
+        missing_files.append(audio_file_path)
+        batch["audio"] = None  # assign None for missing audio files
+    
     return batch
 
 # check if the processed dataset already exists
@@ -36,7 +50,13 @@ else:
     dataset.save_to_disk(processed_data_path)
     print("Processed dataset saved to disk!")
 
-print(dataset[0])
+# save the missing files to a CSV
+if missing_files:
+    missing_files_df = pd.DataFrame(missing_files, columns=["missing_file_path"])
+    missing_files_df.to_csv("../../data_processed/missing_audio_files.csv", index=False)
+    print("Missing audio files saved to 'missing_audio_files.csv'.")
+else:
+    print("No missing audio files detected.")
 
 feature_extractor = WhisperFeatureExtractor.from_pretrained("openai/whisper-small")
 tokenizer = WhisperTokenizer.from_pretrained("openai/whisper-small", language="English", task="transcribe")
@@ -45,7 +65,7 @@ processor = WhisperProcessor.from_pretrained("openai/whisper-small", language="E
 def prepare_dataset(batch):
     audio = batch["audio"]
     # compute log-Mel input features from input audio array 
-    batch["input_features"] = feature_extractor(audio["array"]).input_features[0]
+    batch["input_features"] = feature_extractor(audio["array"], sampling_rate=audio["sampling_rate"]).input_features[0]
 
     # encode target text to label ids 
     batch["labels"] = tokenizer(batch["transcriptions"]).input_ids
