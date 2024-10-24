60da92e99ca9168ba5a5c0c07a2de03fb21ef8ef
Liting <litingzhou1119@outlook.com>
2024-10-24 18:42:47 -0400

    add freezing layers to training script

--- vanilla_training/training.py
+++ vanilla_training/training.py
@@ -1,5 +1,6 @@
 # based on https://huggingface.co/blog/fine-tune-whisper
-# usage example: python3 training.py "small"
+# usage example no freezing: python3 training.py "small"
+# usage example freezing: python3 training.py small --freeze_layers 10
 
 from transformers import WhisperForConditionalGeneration, WhisperProcessor, Seq2SeqTrainingArguments, Seq2SeqTrainer
 from data_collator import DataCollatorSpeechSeq2SeqWithPadding
@@ -13,6 +14,7 @@ import os
 # parse command-line arguments
 parser = argparse.ArgumentParser(description="Train Whisper model with a specified size.")
 parser.add_argument("model_size", type=str, choices=["tiny", "small", "medium", "large"], help="Size of the Whisper model to use.")
+parser.add_argument("--freeze_layers", type=int, default=0, help="Number of encoder layers to freeze, default is 0.")
 args = parser.parse_args()
 
 model_map = {
@@ -40,6 +42,15 @@ print("Data set loaded.")
 model = WhisperForConditionalGeneration.from_pretrained(model_id)
 model.to(device)
 
+# freeze encoder layers if specified
+if args.freeze_layers > 0:
+    print(f"Freezing the first {args.freeze_layers} encoder layers.")
+    for layer_idx in range(args.freeze_layers):
+        for param in model.model.encoder.layers[layer_idx].parameters():
+            param.requires_grad = False
+else:
+    print("No encoder layers are frozen.")
+
 model.generation_config.language = "English"
 model.generation_config.task = "transcribe"
 model.generation_config.forced_decoder_ids = None
@@ -56,10 +67,16 @@ data_collator = DataCollatorSpeechSeq2SeqWithPadding(
 print("Data collator finished.")
 
 # define training arguments based on the model size
-def get_training_args(model_size):
+def get_training_args(model_size, freeze_layers):
+    output_dir = f"../../trained_models/whisper-{model_size}-vanilla"
+
+    # change output directory for the large model with freezing layers greater than 0
+    if model_size == "large" and freeze_layers > 0:
+        output_dir = f"../../trained_models/whisper-large-freezing-{freeze_layers}"
+
     if model_size == "small":
         return Seq2SeqTrainingArguments(
-            output_dir=f"../../trained_models/whisper-{model_size}-vanilla",
+            output_dir=output_dir,
             per_device_train_batch_size=16,
             gradient_accumulation_steps=1,
             learning_rate=1.25e-5,
@@ -83,7 +100,7 @@ def get_training_args(model_size):
         )
     elif model_size == "medium":
         return Seq2SeqTrainingArguments(
-            output_dir=f"../../trained_models/whisper-{model_size}-vanilla",
+            output_dir=output_dir,
             per_device_train_batch_size=8,
             gradient_accumulation_steps=2,
             learning_rate=6.25e-6,
@@ -107,7 +124,7 @@ def get_training_args(model_size):
         )
     elif model_size == "large":
         return Seq2SeqTrainingArguments(
-            output_dir=f"../../trained_models/whisper-{model_size}-vanilla",
+            output_dir=output_dir,
             per_device_train_batch_size=4,  # smaller batch size for larger models
             gradient_accumulation_steps=4,
             learning_rate=5e-6,
@@ -130,7 +147,7 @@ def get_training_args(model_size):
             save_total_limit=5,
         )
 
-training_args = get_training_args(args.model_size)
+training_args = get_training_args(args.model_size,args.freeze_layers)
 
 # check if a checkpoint exists in the output directory
 def get_latest_checkpoint(output_dir):
