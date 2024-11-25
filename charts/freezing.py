2b6e5cab65e79b09a9a03ee5a6f3657cb10019d4
Liting-Zhou <143493320+Liting-Zhou@users.noreply.github.com>
2024-11-25 11:14:02 -0800

    add new data to freezing.py

--- charts/freezing.py
+++ charts/freezing.py
@@ -14,13 +14,13 @@ fontsize_ticks=20
 
 # Data
 encoder_layers = [
-    0, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 
+    0, 5,6,7,8,9,10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 
     21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32
 ]
 wer_values = [
-    15.84, 16.38, 16.46, 16.85, 18.18, None, 17.47, 16.49, 17.8, 18.18, 
-    18.38, 15.93, 18.33, 18.13, 16.16, 19.08, 17.51, 17.72, 17.25, 
-    17.70, 19.31, 17.70, 18.96, 18.51
+    15.84, 16.99,16.55, 16.42, 16.23, 15.87, 16.38, 16.46, 16.85, 18.18, 16.31, 17.47, 16.49, 17.8, 18.18, 
+    18.38, 15.93, 18.27, 18.13, 16.16, 19.08, 17.51, 17.72, 17.25, 
+    17.70, 19.31, 17.70, 18.96, 18.38
 ]
 
 # Filter valid data points
@@ -35,13 +35,13 @@ plt.scatter(valid_layers[0], valid_wer[0], color="#C8102E", s=60, label="First P
 plt.text(valid_layers[0]+0.5, valid_wer[0], f'{valid_wer[0]:.2f}', fontsize=fontsize_ticks, ha='left', color="#C8102E")
 
 # Plot the lowest dot in gold
-plt.scatter(valid_layers[10], valid_wer[10], color="#A4804A", s=60, label="lowest Point")
-plt.text(valid_layers[10]+0.1, valid_wer[10]+0.3, f'{valid_wer[10]:.2f}', fontsize=fontsize_ticks, ha='left', color="#A4804A")
-plt.text(valid_layers[10]+3.5, valid_wer[10]-0.8, f'Layers 1 to {valid_layers[10]} frozen', fontsize=fontsize_ticks, ha='right', color="#A4804A")
+plt.scatter(valid_layers[5], valid_wer[5], color="#A4804A", s=60, label="lowest Point")
+plt.text(valid_layers[5]+0.5, valid_wer[5]-0.3, f'{valid_wer[5]:.2f}', fontsize=fontsize_ticks, ha='left', color="#A4804A")
+plt.text(valid_layers[5]+5.5, valid_wer[5]-1, f'Layers 1 to {valid_layers[5]} frozen', fontsize=fontsize_ticks, ha='right', color="#A4804A")
 
 # Plot the rest of the dots in black
-other_layers = valid_layers[1:10] + valid_layers[11:]
-other_wer = valid_wer[1:10] + valid_wer[11:]
+other_layers = valid_layers[1:5] + valid_layers[6:]
+other_wer = valid_wer[1:5] + valid_wer[6:]
 plt.scatter(other_layers, other_wer, color="#000000", s=60, label="Other Points")
 
 plt.title("WER (%) for Different Number of Bottom Layers Frozen", fontsize=fontsize_title)
@@ -60,4 +60,7 @@ ax.spines['top'].set_visible(False)
 ax.spines['right'].set_visible(False)
 
 plt.tight_layout()
+
+# Save and show
+# plt.savefig("encoder_freezing_dot_chart.png")
 plt.show()
