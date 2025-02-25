839ef0d4a149b3fccf78b792942089d5d5915eab
songjiahui0210 <lynnmarch32013@outlook.com>
2025-02-24 19:17:12 -0500

    added metrics eval

---
+++ training_lora/compute_metrics.py
@@ -0,0 +1,26 @@
+import evaluate
+
+def compute_metrics(pred, tokenizer):
+    """
+    Compute the Word Error Rate (WER) for model predictions.
+    
+    """
+    # Load the WER metric from the `evaluate` library
+    metric = evaluate.load("wer")
+
+    # Extract predictions and label IDs from the output
+    pred_ids = pred.predictions.argmax(-1)
+    label_ids = pred.label_ids
+
+    # Replace -100 in labels ids with tokenizer's pad token ID. -100 is used to ignore indices in loss computations.
+    label_ids[label_ids == -100] = tokenizer.pad_token_id
+
+    # Decode predictions and labels from ids to strings
+    pred_str = tokenizer.batch_decode(pred_ids, skip_special_tokens=True)
+    label_str = tokenizer.batch_decode(label_ids, skip_special_tokens=True)
+
+    # Calculate WER using the `evaluate` library
+    wer_score = metric.compute(predictions=pred_str, references=label_str)
+
+    # Return a dictionary with the WER result
+    return {"wer": wer_score}
