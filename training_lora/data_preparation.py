a56e8b3ea6041219db9113ddd2f6ebd321d7f685
songjiahui0210 <lynnmarch32013@outlook.com>
2025-02-27 01:55:56 -0500

    corrected partition

--- training_lora/data_preparation.py
+++ training_lora/data_preparation.py
@@ -4,14 +4,19 @@ import pandas as pd
 from datasets import Dataset, DatasetDict, load_from_disk
 import os 
 import soundfile as sf
+import pyarrow as pa
+import numpy as np
+
 
 def process_audio_file(batch):
     audio_file_path = os.path.join("../../data_processed/audios", batch["folder_name"], batch["file_cut"])
     if os.path.exists(audio_file_path):
         audio, sample_rate = sf.read(audio_file_path)
-        return {"audio": {"array": audio, "sampling_rate": sample_rate}}
+        audio_list = audio.tolist() if isinstance(audio, np.ndarray) else audio
+        return {"audio": {"array": audio_list, "sampling_rate": int(sample_rate)}}
     else:
         return {"audio": None}
+
 
 def prepare_dataset(dataset, feature_extractor):
     def prepare(batch):
@@ -34,16 +39,16 @@ def main():
     df = pd.read_csv(csv_file_path)
 
     # Calculate duration for each utterance in milliseconds
-    df['utterance_duration'] = df['mark_end'] - df['mark_start']
+    df['utterance_duration'] = (df['mark_end'] - df['mark_start']).astype(int)
     
     # Sum durations by speaker or file
-    speaker_durations = df.groupby('name')['utterance_duration'].sum().reset_index()
+    speaker_durations = df.groupby('name_unique_speaker')['utterance_duration'].sum().reset_index()
 
     # Filter speakers by total duration > 480,000 milliseconds (8 minutes)
-    valid_speakers = speaker_durations[speaker_durations['utterance_duration'] > 480000]['name']
+    valid_speakers = speaker_durations[speaker_durations['utterance_duration'] > 480000]['name_unique_speaker']
     
     # Filter the original dataframe to include only valid speakers
-    df_filtered = df[df['name'].isin(valid_speakers)]
+    df_filtered = df[df['name_unique_speaker'].isin(valid_speakers)]
 
     dataset = Dataset.from_pandas(df_filtered)
     dataset = dataset.map(process_audio_file)
