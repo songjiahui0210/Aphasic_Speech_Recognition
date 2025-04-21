62e0e5407b5df3bea3243529b5aad78a5cccde31
liulian26 <156288189+liulian26@users.noreply.github.com>
2025-04-21 02:16:50 -0700

    Update train_validation.py

--- training_lora/train_validation.py
+++ training_lora/train_validation.py
@@ -17,36 +17,36 @@ def _patched_forward(self, *args, **kwargs):
     return _orig_forward(self, *args, **kwargs)
 PeftModel.forward = _patched_forward
 # --------------------------
-# 1) 环境 & 数据
+# 1) env & data
 # --------------------------
 torch.cuda.empty_cache()
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Using device: {device}")
 
-# Validation 数据集路径
+# Validation 
 eval_ds = load_from_disk("/home/lian/data_processed/eval_dataset_ft_set2_validation_small")
 print(f"Eval dataset size: {len(eval_ds)}")
 
 # --------------------------
-# 2) 加载模型 + adapter
+# 2) loading model + adapter
 # --------------------------
-# 加载基础 Whisper-small
+# loading basic Whisper-small
 base_model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-small").to(device)
 base_model.config.use_cache = False
 
-# 将上一步训练好的 adapter 贴上来
+# add adapter 
 adapter_path = "/home/lian/data_processed/models/lora_personalized_speaker001_small32r64a"
 model = PeftModel.from_pretrained(base_model, adapter_path).to(device)
-model.print_trainable_parameters()  # 这里 trainable% 应该是 0
+model.print_trainable_parameters() 
 
-# Processor 必须和第一次训练 adapter 时用的一致
+# Processor must be smal as the first time training adapter
 processor = WhisperProcessor.from_pretrained(adapter_path, language="en", task="transcribe")
 
 # --------------------------
-# 3) Trainer 只 eval，不 train
+# 3) Trainer only eval，not train
 # --------------------------
 training_args = Seq2SeqTrainingArguments(
-    output_dir="/home/lian/tmp/val_run",  # 不会真的往里存 checkpoint
+    output_dir="/home/lian/tmp/val_run",
     do_train=False,
     do_eval=True,
     per_device_eval_batch_size=1,
@@ -67,7 +67,7 @@ trainer = Seq2SeqTrainer(
 )
 
 # --------------------------
-# 4) 运行 Evaluation
+# 4) run Evaluation
 # --------------------------
 metrics = trainer.evaluate()
 print("===== Validation Results =====")
